4c9deed38bc1ea172fa448731433da075e558569
Ramakrishnan <ramkicse@gmail.com>
2020-09-15 22:14:40 +0530

    Updates

---
+++ precache-manifest.1d1c51d2519e0af6884725be896faee2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a157d81a7cb30f4a361625d45600b317",
+    "url": "/React-DeiveryApp/index.html"
+  },
+  {
+    "revision": "e3cb743e168fede19f7a",
+    "url": "/React-DeiveryApp/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8c465486f3973c095778",
+    "url": "/React-DeiveryApp/static/css/main.ed1bf982.chunk.css"
+  },
+  {
+    "revision": "e3cb743e168fede19f7a",
+    "url": "/React-DeiveryApp/static/js/2.ac5c247c.chunk.js"
+  },
+  {
+    "revision": "74f4e1eda03d9ac324f4b3574b48fc97",
+    "url": "/React-DeiveryApp/static/js/2.ac5c247c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c465486f3973c095778",
+    "url": "/React-DeiveryApp/static/js/main.761ceefc.chunk.js"
+  },
+  {
+    "revision": "eb355381af1b0b2ef26b",
+    "url": "/React-DeiveryApp/static/js/runtime-main.aece2a1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-DeiveryApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
